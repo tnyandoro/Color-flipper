78f44eff754ae3abfe40e673645b026ebc52399d
Tendai Nyandoro <tendai1@hotmail.com>
2021-03-01 22:30:01 +0200

    Add the readme

---
+++ app.js
@@ -0,0 +1,7 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.querySelector("#btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function () {
+  
+})
