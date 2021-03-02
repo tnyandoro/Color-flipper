857e5d4159017b802b13dc3786a0a869edac94d9
Tendai Nyandoro <tendai1@hotmail.com>
2021-03-02 13:50:17 +0200

    Set a value to selecte and affect the back ground

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btn = document.querySelector("#btn");
-const color = document.querySelector(".color");
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
 
 btn.addEventListener('click', function () {
   // We want to get a random number between 0 - 3
-  const randomNumber = 2;
-  document.body.style.backgroundColor = color[randomNumber];
-  color.textContent = colors[randomNumber]
+ const randomNumber = 2;
+ document.body.style.backgroundColor = colors[randomNumber];
+ color.textContent = colors[randomNumber];
 });
