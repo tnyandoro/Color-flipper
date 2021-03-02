f2aa0c631f428a76aaae48acc4f7ad7f5405f603
Tendai Nyandoro <tendai1@hotmail.com>
2021-03-02 14:15:24 +0200

    Add a forloop to run through the hex values

--- hex.js
+++ hex.js
@@ -1,7 +1,12 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+// Set a loop to run six times to select six from the array hex
 const btn = document.querySelector('#btn');
 const color = document.querySelector('.color');
 
 btn.addEventListener('click', function () {
-  
+  let hexColr = '#';
+  for(let i = 0; i < 6, i++){
+    hexColor += hex[0]
+  }
 });
